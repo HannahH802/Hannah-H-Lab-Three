044aaa6ea26b0b8c60542ad0291e78f7ee206b12
HannahH802 <hhornyak29@setoncatholic.org>
2026-09-23 20:14:13 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# this function adds two number
+def add(x,y):
+    print(x + y)
+
+add(6,7)
+
+# this function multiplies two number
+def multiply(x,y):
+    print(x * y)
+
+multiply(6,7)
+
+# this function subtracts two number
+def sub(x,y):
+    print(x - y)
+
+sub(6,7)
+
+# this function divideds two number
+def div(x,y):
+    print(x / y)
+
+div(6,7)
+####################################################
+#_________________________________________________
+
+x= int(input("Enter your first number: "))
+y= int(input("Enter your second number: "))
+add(x,y)
+multiply(x,y)
+sub(x,y)
+div(x,y)
